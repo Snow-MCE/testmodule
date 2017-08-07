7be15831a0adc096e1be1e84e357ff1e8ab9a97b
Snow <mcepengzhigang@163.com>
2017-08-07 15:29:32 +0800

    物料清单维护功能代码提交

--- providerinterface/src/main/java/crossoft/compute/mapper/BomConfigMapper.java
+++ providerinterface/src/main/java/crossoft/compute/mapper/BomConfigMapper.java
@@ -1,17 +1,41 @@
 package crossoft.compute.mapper;
 
 import crossoft.compute.bean.BomBean;
+import crossoft.compute.bean.BomComponentBean;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Snow on 8/3/2017.
  */
 @Repository("bomConfigMapper")
 public interface BomConfigMapper {
+    // 查询物料清单BOM
+    List<BomBean> bomListByBom(Object name);
 
-    public List<BomBean> bomListByBom(Object name);
+    // 新增物料清单BOM
+    void addBom(BomBean bean);
 
-    public void addBom(BomBean bean);
+    // 更新物料清单
+    void updateBom(BomBean bean);
+
+    // 删除物料清单
+    void delteBom(BomBean bean);
+
+    // 查询物料清单组件
+    List<BomComponentBean> bomComponentList(Object name);
+
+    // 新增物料清单组件
+    void addBomComponent(BomComponentBean bean);
+
+    // 删除物料清单组件
+    void deleteBomComponent(BomComponentBean bean);
+
+    // 更新物料清单组件
+    void updateBomComponent(BomComponentBean bean);
+
+    // BOM拆分时组件重新分配
+    void reDistributeBomComponent(Map map);
 }
