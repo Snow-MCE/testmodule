803fbb63cefedef1555ae8609ca4e2c618446bba
Snow <mcepengzhigang@163.com>
2017-08-07 15:31:47 +0800

    物料清单维护功能代码提交

--- provider/src/main/java/crossoft/compute/impl/controller/BomConfigController.java
+++ provider/src/main/java/crossoft/compute/impl/controller/BomConfigController.java
@@ -1,11 +1,15 @@
 package crossoft.compute.impl.controller;
 
 import crossoft.compute.bean.BomBean;
+import crossoft.compute.bean.BomComponentBean;
 import crossoft.compute.mapper.BomConfigMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Snow on 8/3/2017.
@@ -31,6 +35,40 @@ public class BomConfigController {
         bomBean.setVersion("A");
         bomBean.setName("function 测试物料");
         bomConfigMapper.addBom(bomBean);
+    }
+
+    public void addComponent() {
+        BomComponentBean componentBean = new BomComponentBean();
+        componentBean.setId("BomComponent,60,ITEM001");
+        componentBean.setSequence(new BigDecimal(60));
+        componentBean.setAssembleOperationFk("OPERATION,TEST,OPER002");
+        componentBean.setBomFk("BOM,TEST,ITEM001");
+        componentBean.setBomComponentType("测试");
+        componentBean.setDisassembleOperationFk("OPERATION,TEST,OPER008");
+        componentBean.setAssyDataTypeFk("BOM_TEST");
+        componentBean.setMaximumNc((double) 46);
+        componentBean.setMaterialFk("BOM002,TEST,A");
+        componentBean.setMaximumUsage(35.876);
+        componentBean.setQty( 34.765 );
+        componentBean.setAssembleAsReq("Y");
+        componentBean.setEnabled("Y");
+        componentBean.setUseItemDefaults("A");
+        componentBean.setTestPart("ITEM0080");
+        bomConfigMapper.addBomComponent(componentBean);
+    }
+
+    public void delteComponent(){
+        BomComponentBean componentBean = new BomComponentBean();
+        componentBean.setId("BomComponent,60,ITEM001");
+        bomConfigMapper.deleteBomComponent(componentBean);
+    }
+
+    public void reDisBomComp(){
+        Map map = new HashMap<String,String>();
+        map.put("newId","BomComponent,50,ITEM003");
+        map.put("newBomFk","BOM,TEST,ITEM003");
+        map.put("sourceId","BomComponent,50,ITEM002");
+        bomConfigMapper.reDistributeBomComponent(map);
     }
 
 }
